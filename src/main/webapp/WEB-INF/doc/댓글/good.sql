faa3b5367efb01e765a5a7d226519684b19e8926
yegoo8 <kodokim418@gmail.com>
2023-07-05 18:29:47 +0900

    update

--- src/main/webapp/WEB-INF/doc/댓글/good.sql
+++ src/main/webapp/WEB-INF/doc/댓글/good.sql
@@ -12,13 +12,14 @@ CREATE TABLE good(
   FOREIGN KEY (memberno) REFERENCES member (memberno)
 );
 
-COMMENT ON TABLE like is '좋아요';
-COMMENT ON COLUMN like.likeno is '좋아요번호';
-COMMENT ON COLUMN like.fboardno is '공지사항 번호';
-COMMENT ON COLUMN like.memberno is '회원 번호';
-COMMENT ON COLUMN like.rdate is '등록일';
+COMMENT ON TABLE good is '좋아요';
+COMMENT ON COLUMN good.likeno is '좋아요번호';
+COMMENT ON COLUMN good.fboardno is '자유게시판 번호';
+COMMENT ON COLUMN good.memberno is '회원 번호';
+COMMENT ON COLUMN good.rdate is '등록일';
 
 DROP SEQUENCE good_seq;
+
 CREATE SEQUENCE good_seq
   START WITH 1              -- 시작 번호
   INCREMENT BY 1          -- 증가값
@@ -27,4 +28,26 @@ CREATE SEQUENCE good_seq
   NOCYCLE;                   -- 다시 1부터 생성되는 것을 방지
   
 INSERT INTO good(goodno, fboardno, memberno, rdate)
-VALUES(reply_seq.nextval, 5, 3, '댓글1', '1234', sysdate);
+VALUES(good_seq.nextval, 1, 3, sysdate);
+
+commit;
+
+SELECT goodno FROM good;
+
+DELETE GOOD
+WHERE fboardno=1
+AND memberno=3
+
+SELECT BB.fboardno, G.memberno, NVL(GC.gcnt,0) AS gcnt
+FROM fboard BB LEFT OUTER JOIN good G
+                                  ON BB.fboardno = G.fboardno
+                                  AND G.memberno= 3  
+                     LEFT OUTER JOIN (SELECT COUNT(*) AS gcnt, fboardno
+                                      FROM good 
+                                      WHERE fboardno= 1
+                                      GROUP BY fboardno) GC
+                                  ON BB.fboardno = GC.fboardno
+WHERE BB.fboardno = 1;
+
+좋아요 했는지 확인
+SELECT COUNT(*) FROM good WHERE fboardno = 1 AND memberno = 3;
